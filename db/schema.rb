494ecdd159f9c601e7df7945c8dbf476f83d0e16
Vitalik Stetsenko <vitalikstetsenko@hotmail.com>
2016-01-08 22:49:50 +0200

    sign in & log in & log out

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20160108160616) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
