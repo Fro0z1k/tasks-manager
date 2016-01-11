dc2fb6500533f0d3feeb1462121504ba0b5ecaa5
Vitalik Stetsenko <vitalikstetsenko@hotmail.com>
2016-01-12 01:32:34 +0200

    remove scope(priority) ENUM == scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,6 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :user, foreign_key: :owner, class_name: User
 
-  scope :low,         -> { where(priority: 0) }
-  scope :middle,      -> { where(priority: 1) }
-  scope :warning,     -> { where(priority: 2) }
   scope :task_new,    -> { where(status: 'new') }
   scope :progress,    -> { where(status: 'progress') }
   scope :done,        -> { where(status: 'done') }
