cd5d99303c6cb42ba37d76a7ede5dd23f7fe205f
Vitalik Stetsenko <vitalikstetsenko@hotmail.com>
2016-01-11 23:54:40 +0200

    added ENUM

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :user, foreign_key: :owner, class_name: User
 
-  scope :low,     -> { where(priority: 1) }
-  scope :middle,  -> { where(priority: 2) }
-  scope :warning, -> { where(priority: 3) }
+  scope :low,     -> { where(priority: 0) }
+  scope :middle,  -> { where(priority: 1) }
+  scope :warning, -> { where(priority: 2) }
+
+  enum priority: [ :low, :middle, :warning ]
 end
