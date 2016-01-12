94ea5b2e4746863791598cabbc7b6c8138a924d9
Vitalik Stetsenko <vitalikstetsenko@hotmail.com>
2016-01-12 16:44:51 +0200

    validates for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :user, foreign_key: :owner, class_name: User
 
+  validates :name, :text, presence: true
+
   scope :task_new,    -> { where(status: 'new') }
   scope :progress,    -> { where(status: 'progress') }
   scope :done,        -> { where(status: 'done') }
